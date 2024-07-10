434996cfbfb63fdd2da40b716ebaaeaf18c95062
Visalatchi-rvs <visalatchimec@gmail.com>
2024-07-10 03:44:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var on_button=document.getElementById("on");
+var off_button=document.getElementById("off");
+var image=document.getElementById("bulb");
+
+function bulb_on() {
+    image.src="./images/bulb_on.jpeg"
+}
+function bulb_off() {
+    image.src="./images/bulb_off.jpeg"
+}
